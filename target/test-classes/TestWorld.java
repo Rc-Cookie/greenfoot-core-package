271aa626a1c1bd807ce4224cdf84ada6298e5991
RcCookie0 <70144256+RcCookie0@users.noreply.github.com>
2021-02-03 09:53:30 +0100

    Build update

--- target/test-classes/TestWorld.java
+++ target/test-classes/TestWorld.java
@@ -10,7 +10,7 @@ public class TestWorld extends CoreWorld {
     
     public TestWorld() {
         super(600, 400);
-        addRelative(Image.text(">> Hello\n   World!\nThis is a monospace font, 'Consolas', used for coding.", Color.RED, FontStyle.monospace(16)).asActor(), 0.5, 0.1);
+        addRelative(Image.text(">> Hello\n   World!\nThis is a monospace font, 'Consolas', used for coding.\nIt's the default editor font in Visual Studio Code.", Color.RED, FontStyle.monospace(16)).asActor(), 0.5, 0.1);
         addRelative(Image.text(">> Hello\n   World!\nThis is a modern font, 'Segoe UI', nice for paragraphs.", Color.RED, FontStyle.modern(16)).asActor(), 0.5, 0.3);
         //addRelative(Image.text(">> Hello\n   World!\nThis is a proper text, longer than just two words.\n", Color.RED, new Font("Candera", false, false, 16)).asActor(), 0.5, 0.5);
         //addRelative(Image.text(">> Hello\n   World!\nThis is a proper text, longer than just two words.\n", Color.RED, new Font("Calibri", false, false, 16)).asActor(), 0.5, 0.7);
